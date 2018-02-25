a18a4ff693d1b38113d9b20b684b05c668c11383
Didja09 <khalal.didja09@gmail.com>
2018-02-25 21:58:52 +0100

    25/02/2018 22.00

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel oussama
+                    Laravel ma3za
                 </div>
 
                 <div class="links">
